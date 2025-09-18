07fb3ae1c9a89cb63e142757f7fa330105f0448c
Guilherme Pereira Pardim <guilherme.p.pardim@gmail.com>
2025-09-18 15:16:15 -0300

    Update to_do.py

--- to_do.py
+++ to_do.py
@@ -6,4 +6,21 @@ import os
 arquivos_dados = "dados_user.json"
 
 def carregar_dados():
-    if not os.path.exists
+    if not os.path.exists(arquivos_dados):
+        return {}
+    try:
+        with open(arquivos_dados, 'r') as f:
+            return json.load(f)
+    
+    except (json.jsondecodeerror, FileNotFoundError):
+        return{}
+
+def save_data (dados):
+    
+    with open (arquivos_dados, 'w') as f:
+        json.dump(dados, f indent = 4)
+        
+def hash_senha (senha) :
+    return hashlib.sha256(senha.encode()) . hexdigest()
+
+def registrar_usuario (dados)       
